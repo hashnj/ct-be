5249788a706ee201948d2443fac72a979cb91845
hashnj <joshinikhilesh46@gmail.com>
2024-07-30 18:24:37 +0530

    changesss

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,12 +3,14 @@ const jwt_secret='secret';
 
 
 const authenticateToken = (req, res, next) => {
-    const token = req.header('Authorization')?.split(' ')[1];
+    // console.log(req.header)
+    const token = req.header('authorization')?.split(' ')[1];
+    // console.log(token);
     if (!token) return res.status(401).json({ error: 'Access denied. No token provided.' });
 
     try {
         const decoded = jwt.verify(token, jwt_secret);
-        console.log(decoded);
+        // console.log(decoded);
         req.user = decoded;
         next();
     } catch (err) {
--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { Products, ProductImages, Categories, SubCategories, Vendors, WishList, Cart } = require('../db');
 const jwt = require('jsonwebtoken');
-const z = require('zod');
 const authenticateToken = require('../middlewares/auth');
 
 const ProductRouter = express.Router();
@@ -9,57 +8,44 @@ const ProductRouter = express.Router();
 const jwt_secret = 'secret';
 
 ProductRouter.post('/add', async (req, res) => {
-    console.log('dikkat')
-    console.log(req.body);
+    const { body, headers } = req;
+    const authHeader = headers.authorization;
 
-    const body = req.body;
-    const headers = req.headers.authorization;
-    console.log(headers,'headers');
-
-    if (!headers) {
+    if (!authHeader) {
         return res.status(401).json('Provide auth key');
     }
 
-    const token = headers.split(' ')[1];
+    const token = authHeader.split(' ')[1];
     let decodedToken;
 
     try {
         decodedToken = jwt.verify(token, jwt_secret);
-        console.log(decodedToken,"token");
     } catch (err) {
         return res.status(401).json('Invalid auth key');
     }
 
     let category_id;
-    if (body.product.category) {
-        const cat = await Categories.findOne({ name: body.product.category });
-        const subCat = await SubCategories.findOne({ name:body.product.category });
-        if (cat) {
-            console.log(cat._id,"2");
-            category_id = cat._id;
-        } else if(subCat){
-            console.log(subCat._id,'3');
-            category_id =  subCat.parent_id;
-            const catt = await Categories.findById(category_id);
-            console.log(catt,"jm",category_id)
-        }
-        else{
-            console.error("Category not found");
+    try {
+        const category = await Categories.findOne({ name: body.product.category });
+        const subCategory = await SubCategories.findOne({ name: body.product.category });
+
+        if (category) {
+            category_id = category._id;
+        } else if (subCategory) {
+            category_id = subCategory.parent_id;
+        } else {
             return res.status(404).json({ error: 'Category not found' });
         }
-    }
 
-    try {
         const product = await Products.create({
             category_id,
             vendor_id: decodedToken.userId,
             name: body.product.name,
             description: body.product.description,
-            mrp:body.product.mrp,
+            mrp: body.product.mrp,
             price: body.product.price,
-            stock: body.product.stock
+            stock: body.product.stock,
         });
-        console.log(product)
 
         const img = await ProductImages.create({
             product_id: product._id,
@@ -68,118 +54,84 @@ ProductRouter.post('/add', async (req, res) => {
 
         res.status(201).json({ product, img });
     } catch (error) {
-        console.log(error+'  this');
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
+ProductRouter.put('/', authenticateToken, async (req, res) => {
+    const { name, description, mrp, price, stock, image } = req.body.item;
+    try {
+        const updated = await Products.findOneAndUpdate(
+            { $or: [{ name }, { description }] },
+            { name, description, mrp, price, stock }
+        );
+        await ProductImages.findOneAndUpdate(
+            { product_id: updated._id },
+            { image_url: image[0] }
+        );
+        res.json({ yay: "yay" });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
-
-ProductRouter.put('/',authenticateToken,async ( req , res ) => {
-    const {name,description,mrp,price,stock,image}=req.body.item;
-    try{
-    const updated= await Products.findOneAndUpdate({$or:[
-        {name:name},
-        {description:description}
-    ]},{
-        name,
-        description,
-        mrp,
-        price,
-        stock
-    });
-    const up=await ProductImages.findOneAndUpdate({
-        product_id:updated._id
-    },{
-      image_url:image[0] 
-    })
-    console.log(updated);
-    return res.json({yay:"yay"});
-}
-catch(e){
-    console.log(e);
-}
-})
-
-
-ProductRouter.post('/cart', authenticateToken , async (req,res)=>{
-    try{
-        const body=req.body;
-        const user=req.user;
+ProductRouter.post('/cart', authenticateToken, async (req, res) => {
+    try {
+        const { list } = req.body;
+        const { userId } = req.user;
         
-        const exists = await Cart.findOne({user_id:user.userId});
-        let qry;
-        if(exists){
-            qry =await Cart.findOneAndUpdate({user_id:user.userId},{product_id:body.list})
-        }
-        else{
-        qry= await Cart.create({user_id:user.userId,product_id:body.list});
-        }
-        console.log(qry);
-        return res.json({qry});
-    }
-    catch(e){
-        console.log(e);
-    }
-})
+        const exists = await Cart.findOne({ user_id: userId });
+        const qry = exists
+            ? await Cart.findOneAndUpdate({ user_id: userId }, { product_id: list })
+            : await Cart.create({ user_id: userId, product_id: list });
 
+        res.json({ qry });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
-ProductRouter.get('/cart', authenticateToken , async (req,res)=>{
-    try{
-        
-        const user=req.user;
-        
-        const qry= await Cart.find({user_id:user.userId});
-        console.log(qry);
-        return res.json({qry});
+ProductRouter.get('/cart', authenticateToken, async (req, res) => {
+    try {
+        const { userId } = req.user;
+        const qry = await Cart.find({ user_id: userId });
+        res.json({ qry });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-    catch(e){
-        console.log(e);
-    }
-})
-ProductRouter.post('/wish', authenticateToken , async (req,res)=>{
-    try{
-        const body=req.body;
-        const user=req.user;
-        
-        const exists = await Cart.findOne({user_id:user.userId});
-        let qry;
-        if(exists){
-            qry =await Cart.findOneAndUpdate({user_id:user.userId},{product_id:body.list})
-        }
-        else{
-        qry= await Cart.create({user_id:user.userId,product_id:body.list});
-        }
-        console.log(qry);
-        return res.json({qry});
-    }
-    catch(e){
-        console.log(e);
-    }
-})
+});
 
+ProductRouter.post('/wish', authenticateToken, async (req, res) => {
+    try {
+        const { list } = req.body;
+        const { userId } = req.user;
+        // console.log(list,userId);
+        const exists = await WishList.findOne({ user_id: userId });
+        const qry = exists
+            ? await WishList.findOneAndUpdate({ user_id: userId }, { product_id: list })
+            : await WishList.create({ user_id: userId, product_id: list });
 
-ProductRouter.get('/wish', authenticateToken , async (req,res)=>{
-    try{
-        
-        const user=req.user;
-        
-        const qry= await WishList.find({user_id:user.userId});
-        console.log(qry);
-        return res.json({qry});
+        res.json({ qry });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-    catch(e){
-        console.log(e);
-    }
-})
+});
 
+ProductRouter.get('/wish', authenticateToken, async (req, res) => {
+    try {
+        const { userId } = req.user;
+        const qry = await WishList.find({ user_id: userId });
+        res.json({ qry });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 ProductRouter.get('/', async (req, res) => {
     try {
         const products = await Products.find({})
             .populate('category_id', 'name')
-            .populate('vendor_id','business_name')
-            
+            .populate('vendor_id', 'business_name');
 
         const productsWithImages = await Promise.all(products.map(async (product) => {
             const images = await ProductImages.find({ product_id: product._id });
@@ -188,14 +140,11 @@ ProductRouter.get('/', async (req, res) => {
                 images: images.map(img => img.image_url)
             };
         }));
-        res.status(200).json({ data: productsWithImages });
 
+        res.status(200).json({ data: productsWithImages });
     } catch (error) {
-        console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
-
 });
 
 module.exports = ProductRouter;
-
